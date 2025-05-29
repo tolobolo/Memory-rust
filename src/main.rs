ba068347e31d5fca28ba9590349c9c6038ac7806
Emrys <emrys@local>
2025-05-29 16:37:04 +0200

    start on the cards

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use rand::rng;
+use rand::seq::SliceRandom;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Memory::new();
+    game.steps()
+}
+
+#[derive(Debug, EnumIter, Clone)]
+enum Card {
+    Box,
+    Apple,
+    Dog,
+    Orange,
+    Kiwi,
+    Pogo,
+    TRex,
+}
+
+struct Memory {
+    tabell: Vec<Card>,
+}
+
+impl Memory {
+    fn new() -> Self {
+        let mut tabell: Vec<Card> = Vec::new();
+        for card in Card::iter() {
+            tabell.push(card.clone());
+            tabell.push(card);
+        }
+
+        Self { tabell }
+    }
+
+    fn sort_tabell(&mut self) {
+        let mut rng = rng();
+        self.tabell.shuffle(&mut rng);
+    }
+    fn print_tabell(&self) {
+        for (index, num) in self.tabell.iter().enumerate() {
+            print!(" {} ", index)
+        }
+        println!()
+    }
+
+    fn steps(&mut self) {
+        self.sort_tabell();
+        dbg!(&self.tabell);
+        self.print_tabell();
+    }
 }
